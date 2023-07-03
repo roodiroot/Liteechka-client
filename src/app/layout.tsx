8b2f4efba2450fda12cc63a030dc790bff20e1f7
Borisov Maxim <voodivood@gmail.com>
2023-07-03 19:56:00 +0300

    prod030723

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        <link href="favicon.ico" rel="icon"></link>
+        <meta name="yandex-verification" content="c5d3873f351893a5" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+            ym(94178621, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+         });
+              `,
+          }}
+        />
+      </head>
       <body
         className={`relative w-full h-full min-h-screen flex flex-col ${inter.className}`}
       >
